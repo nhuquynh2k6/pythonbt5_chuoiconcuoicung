284fd40d98b4be8796fd6cdf488960fe128909ce
nhuquynh2k6 <nhuquynhh2k6@gmail.com>
2025-11-16 15:18:32 +0700

    Tim chuoi con cuoi cung

--- pythonbt4.py
+++
@@ -1,5 +0,0 @@
-chuoi=input("Nhap chuoi")
-digits=[int(ch) for ch in chuoi if ch.isdigit()]
-tong=sum(digits)
-trung_binh=tong/len(digits) if digits else 0
-print(f"Tong la {tong} va trung binh la {trung_binh}")
